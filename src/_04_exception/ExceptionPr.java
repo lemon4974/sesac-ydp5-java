bd87cbbd47741c57d491ac884fdc0e3449047c17
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2023-11-23 17:53:17 +0900

    study: exception practice

--- src/_04_exception/ExceptionPr.java
+++ src/_04_exception/ExceptionPr.java
@@ -20,7 +20,7 @@ public class ExceptionPr {
 
     public static void main(String[] args) {
 
-        // 실습 1.
+        // 실습 1. 크기가 4인 배열, 인덱스가 범위를 벗어난 예외 처리
         int[] numbers = {1, 2, 3, 4};
 
         try {
@@ -32,18 +32,43 @@ public class ExceptionPr {
         }
 
 
-        // 실습 2.
+        // 실습 2. 배열 크기 입력 받아 배열 요소의 평균 계산
 
-//        Scanner sc = new Scanner(System.in);
-//
-//        try {
-//            System.out.println("배열의 크기를 입력하세요: ");
-//            int number = sc.nextInt();
-//            System.out.println("입력한 정수 : " + number);
-//        } catch (InputMismatchException e) {
-//            System.out.println("에러 발생 >> " + e.toString());
-//        }
+        try {
 
-        // 실습 3.
+            System.out.print("배열의 크기를 입력해주세요 : ");
+            Scanner sc = new Scanner(System.in);
+            int arrLen = sc.nextInt();
+
+
+            if (arrLen < 0 || arrLen > 20) {
+                throw new IllegalArgumentException("Invalid array length");
+            }
+
+            int[] array = new int[arrLen];
+
+            System.out.println("");
+            System.out.println("20개 이하의 숫자 값을 공백으로 구분하여 입력해주세요 : ");
+            for(int i = 0; i < 20; i++){
+                array[i] = sc.nextInt();
+            }
+            int sum=0;
+
+            for(int j = 0; j < arrLen; j++){
+                sum +=array[j];
+            }
+
+            System.out.println("입력 받은 배열의 평균은 " + (double) sum/arrLen +" 입니다.");
+        } catch (ArithmeticException e) {
+            System.out.println("배열의 크기가 0인 경우, 평균을 구할 수 없습니다.");
+        } catch (InputMismatchException e) {
+            System.out.println("배열의 크기는 0보다 큰 자연수여야 합니다. 에러 메세지 : " + e.toString());
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 배열 길이: " + e.getMessage());
+        }
+
+        // 실습 3. 배열의 크기 입력 받아, 중복되는 경우 찾아 출력, 예외 처리
+
+
     }
 }
